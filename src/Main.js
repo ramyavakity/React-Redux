1cff48125f9ee6b26aae3f83a1463fc496858ba7
Ramya Vakity <vakityramya1@gmail.com>
2020-03-04 15:39:20 -0500

    Intergared Store to Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const AppBundle = () => (
+    <Provider store = { store }>
+        <HashRouter>
+            <App/>
+        </HashRouter>
+    </Provider>
+);
+ReactDOM.render(<AppBundle />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

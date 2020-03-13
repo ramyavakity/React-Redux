776ade94c1b71fb64a910a0c4e7978c7d7173564
Ramya Vakity <vakityramya1@gmail.com>
2020-03-13 15:21:14 -0400

    Added CncThemeProvider

---
+++ src/components/connect-nav/CncThemeProvider/CncThemeProvider.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ThemeProvider, StylesProvider, createMuiTheme } from '@material-ui/core/styles';
+import { lightTheme, darkTheme } from '';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const CncThemeProvider = ({ childern, themeName, generateClassName }) => {
+    const theme = createMuiTheme(themeName === 'dark' ? darkTheme : lightTheme)
+    return (
+        <StylesProvider injectFirst generateClassName={generateClassName}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline>{childern}</CssBaseline>
+            </ThemeProvider>
+        </StylesProvider>
+    )
+}
+
+CncThemeProvider.PropTypes = {
+    childern: PropTypes.any,
+    themeName: PropTypes.string,
+    generateClassName: PropTypes.func
+}
+
+//ships with light theme for default 
+CncThemeProvider.defaultProps = {
+    theme: lightTheme
+}
+
+export default CncThemeProvider;

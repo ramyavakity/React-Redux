31fa6661eda8ef641c6acdc17936e6fff5f27598
Ramya Vakity <vakityramya1@gmail.com>
2020-03-13 16:15:25 -0400

    Importing components to App.js and Routes.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,68 @@
 import React, { Component } from 'react';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { lightTheme, darkTheme } from './components/connect-theme';
+import Routes from './Routes';
+import Canvas from './components/connect-nav/Canvas';
+import CncThemeProvider from './components/connect-nav/CncThemeProvider';
+import './index.css';
 
+const GlobalStyle = createGlobalStyle`
+@import url(' https://fonts.googleapis.com/css?family=Lato:300,400,700 ');
+
+html {
+  font-family: 'Lato',helvetica,arial,sans-serif;
+}
+:root {
+  font-size: 100%;
+}
+`
+const AppWrapper = styled.div`
+  height: 100vh;
+  display: grid;
+  -ms-grid-columns: 1fr;
+  grid-template-columns: 1fr;
+  -ms-grid-rows: 56px 1fr;
+  grid-template-rows: 56px 1fr;
+  grid-template-areas: 'TopNav TopNav' 'Canvas Canvas';
+  overflow: hidden;
+`
 class App extends Component {
   render() {
+    const { theme } = this.props;
+    console.log('theme', theme);
     return (
-      <div className="App">
-        <header className="App-header">
-          <h2>Hello World!!!!</h2>
-        </header>
+      <div>
+        <GlobalStyle />
+        <CncThemeProvider themeName={!theme ? 'light' : 'dark'}>
+          <ThemeProvider theme={theme ? lightTheme : darkTheme}>
+            <BrowserRouter>
+              <AppWrapper>
+                <Canvas>
+                  <Routes />
+                </Canvas>
+              </AppWrapper>
+            </BrowserRouter>
+          </ThemeProvider>
+        </CncThemeProvider>
       </div>
-    )
+    );
   }
 }
 
-export default App;
+App.propTypes = {
+  theme: PropTypes.bool
+}
+
+const mapStateToProps = state => {
+  console.log('state', state);
+  return ({
+    theme: state.settings.theme || false
+  })
+}
+
+export default connect(
+  mapStateToProps
+)(App);
---
+++ src/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Settings from './components/connect-nav/Settings';
+
+const Main = () => {
+    <h1 style={{ textAlign: 'center', color: 'darkcyan' }}>Welcome to my React Application</h1>
+}
+
+const Home = () => {
+    <h1></h1>
+}
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route exact path='/' component={Main} />
+            <Route exact path='/home' component={Home} />
+            <Route exact path='/settings' component={Settings} />
+        </Switch>
+    );
+};
+
+export default Routes;

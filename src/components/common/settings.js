421997de18b53dd380c0e2323eb88ea3c156877e
Ramya Vakity <vakityramya1@gmail.com>
2020-03-13 15:00:46 -0400

    Added Settings.js

---
+++ src/components/common/settings.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+import { Switch, Paper, FormControlLabel, Typography } from '@material-ui/core';
+import {fetchSettingsSuccess} from '';
+import PropTypes from 'prop-types';

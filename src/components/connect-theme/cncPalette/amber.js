29c45aae17105f3d53a84922a3430de68bd89a09
Ramya Vakity <vakityramya1@gmail.com>
2020-03-26 12:12:33 -0400

    amber and blue colors

--- src/components/connect-theme/cncPalette/amber.js
+++ src/components/connect-theme/cncPalette/amber.js
@@ -1,8 +1,18 @@
 const amber = {
     50: '#fff8e1',
-    100: '#fffecb3',
-    
-
+    100: '#ffecb3',
+    200: '#ffe082',
+    300: '#ffd54f',
+    400: '#ffca28',
+    500: '#ffc107',
+    600: '#ffb300',
+    700: '#ffa000',
+    800: '#ff8f00',
+    900: '3ff6f00',
+    A100: '#FFE082',
+    A200: '#FFCA28',
+    A400: '#FFC107',
+    A700: '#FFA000'
 }
 
 module.exports = amber

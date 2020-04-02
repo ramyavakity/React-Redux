9fabffc0ebca3c609cf0dc024feb34c31b247b61
Ramya Vakity <vakityramya1@gmail.com>
2020-04-02 17:22:17 -0400

    CncThemeProvider.js - imports

--- src/components/connect-nav/CncThemeProvider/CncThemeProvider.js
+++ src/components/connect-nav/CncThemeProvider/CncThemeProvider.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ThemeProvider, StylesProvider, createMuiTheme } from '@material-ui/core/styles';
-import { lightTheme, darkTheme } from '';
+import { lightTheme, darkTheme } from '../../connect-theme/index';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 const CncThemeProvider = ({ childern, themeName, generateClassName }) => {
+    console.log('cnc',themeName);
     const theme = createMuiTheme(themeName === 'dark' ? darkTheme : lightTheme)
     return (
         <StylesProvider injectFirst generateClassName={generateClassName}>

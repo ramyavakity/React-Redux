997d839ff1da798deb644193ec4aebbac8c4a808
Ramya Vakity <vakityramya1@gmail.com>
2020-04-02 13:48:59 -0400

    Settings.js

--- src/components/connect-nav/Settings/Settings.js
+++ src/components/connect-nav/Settings/Settings.js
@@ -1,12 +1,89 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
-import { Switch, Paper, FormControlLabel, Typography } from '@material-ui/core';
-import {fetchSettingsSuccess} from '';
+import Switch from '../../../../node_modules/@material-ui/core/Switch';
+import Paper from '../../../../node_modules/@material-ui/core/Paper';
+import FormControlLabel from '../../../../node_modules/@material-ui/core/FormControlLabel';
+import Typography from '../../../../node_modules/@material-ui/core/Typography';
+import { fetchSettingsSuccess } from '../../connect-navmiddleware/settings.js';
 import PropTypes from 'prop-types';
 
-export class Settings extends Component {
+const Wrapper = styled.div``
+const StyledFormControlLabel = styled(FormControlLabel)`
+    margin:auto;
+`
+const Container = styles(Paper)`
+   display:grid;
+   gird-template-rows:40px auto;
+   max-width:600px;
+   margin:100px auto;
+`
+const Header = styled.div`
+   display:grid;
+   align-items:center;
+   height:40px;
+   border-bottom:1px solid ${p => p.theme.palette.background.border};
+ 
+`
+const Board = styled.div`
+   height:200px;
+   padding-top:16px;
+`
 
+export class Settings extends Component {
+    handleChange = name => event => {
+        this.props.onToggleChange({ [name]: event.target.checked })
+    }
+
+    rendere() {
+        const { theme } = this.props
+        console.log('theme', theme);
+
+        return (
+            <div>
+                <Container elevation={3}>
+                    <Header>
+                        <Typography style={{ paddingLeft: 16 }} variant="subtitle2">
+                            Settings
+                        </Typography>
+                    </Header>
+
+                    <Board>
+                        <Wrapper>
+                            <StyledFormControlLabel
+                                control={
+                                    <Switch
+                                        checked={theme}
+                                        value={theme}
+                                        onChange={this.handleChange('theme')}
+                                        color="primary"
+                                    />
+                                }
+                                label="Theme"
+                            />
+                        </Wrapper>
+                    </Board>
+                </Container>
+            </div>
+        )
+    }
 }
 
-export default connect()(Settings)
+const mapStateToProps = state => ({
+    language: state.settings.language,
+    theme: state.settings.theme
+})
+
+const mapDispatchToProps = dispatch => ({
+    onToggleChange: payload => dispatch(fetchSettingsSuccess(payload))
+})
+
+Settings.PropTypes = {
+    onToggleChange: PropTypes.func,
+    language: PropTypes.bool,
+    theme: PropTypes.bool
+}
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Settings)

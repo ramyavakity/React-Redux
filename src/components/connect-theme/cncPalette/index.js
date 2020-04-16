4ab06015ee65f3b6a20564525040c7d044d7b2df
Ramya Vakity <vakityramya1@gmail.com>
2020-04-16 15:09:41 -0400

    CncPalette - Index.js

---
+++ src/components/connect-theme/cncPalette/index.js
@@ -0,0 +1,33 @@
+const amber = require('./amber');
+const blue = require('./blue');
+const blueGrey = require('./blueGrey');
+const cyan = require('./cyan');
+const deepOrange = require('./deepOrange');
+const deepPurple = require('./deepPurple');
+const green = require('./green');
+const grey = require('./grey');
+const indigo = require('./indigo');
+const orange = require('./orange');
+const pink = require('./pink');
+const purple = require('./purple');
+const red = require('./red');
+const teal = require('./teal');
+const yellow = require('./yellow');
+
+module.exports = {
+    amber: amber,
+    blue: blue,
+    blueGrey: blueGrey,
+    cyan: cyan,
+    deepOrange: deepOrange,
+    deepPurple: deepPurple,
+    green: green,
+    grey: grey,
+    indigo: indigo,
+    orange: orange,
+    pink: pink,
+    purple: purple,
+    red: red,
+    teal: teal,
+    yellow: yellow
+}

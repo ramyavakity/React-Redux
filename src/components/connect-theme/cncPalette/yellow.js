18386608209a86ea81f14452efd1fdba3cbdff49
Ramya Vakity <vakityramya1@gmail.com>
2020-04-23 21:35:35 -0400

    Red , Teal , Yellow - Complete

---
+++ src/components/connect-theme/cncPalette/yellow.js
@@ -0,0 +1,18 @@
+const yellow = {
+    50: '#fffde7',
+    100: '#fff9c4',
+    200: '#fff59d',
+    300: '#fff176',
+    400: '#ffee58',
+    500: '#ff3b3b',
+    600: '#fdd835',
+    700: '#fbc02d',
+    800: '#f9a825',
+    900: '#f57f17',
+    A100: '#FFF59D',
+    A200: '#FFF176',
+    A400: '#FFEB3B',
+    A700: '#FDD835'
+}
+
+module.exports = yellow
